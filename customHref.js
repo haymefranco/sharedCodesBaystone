30265c3e87da9a65295e9c86c3cd50e23590a985
haymefranco <franclin.cr54rep@gmail.com>
2019-04-15 15:51:58 -0400

    SEARCH A SPECIFIC LINK AND ADD / REMOVE THE FIRST WORDS ON HREF

---
+++ customHref.js
@@ -0,0 +1,17 @@
+
+
+
+
+// SEARCH A SPECIFIC LINK AND ADD / REMOVE THE FIRST WORDS ON HREF
+$(".wrap__page-content a").each(function(){
+    var textHref = $(this).attr('href');
+    var x = textHref.split('0014230/', 2);
+    var y = '';
+
+    if(x[0] == '//my.theonlinepractice.com/'){
+        x[0] = y;
+        var q = $(this).attr('href', x[0] + x[1]);
+        console.log(x);
+    }
+
+});
